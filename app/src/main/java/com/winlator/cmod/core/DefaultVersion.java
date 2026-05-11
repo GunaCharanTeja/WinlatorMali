0ad5f87ead8226bfe1a4980af94c46dc942f4bdb
teja4 <teja44951@gmail.com>
2026-05-11 22:11:57 +0530

    fex,box64,wow64 updates

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -1,9 +1,9 @@
 package com.winlator.cmod.core;
 
 public abstract class DefaultVersion {
-    public static final String BOX64 = "0.3.7";
-    public static final String WOWBOX64 = "0.3.7";
-    public static final String FEXCORE = "2508";
+    public static final String BOX64 = "0.4.1";
+    public static final String WOWBOX64 = "0.4.1";
+    public static final String FEXCORE = "2601";
     public static final String WRAPPER = "System";
     public static final String WRAPPER_ADRENO = "turnip25.1.0";
     public static final String DXVK = GPUInformation.getRenderer(null, null).contains("Mali") ? "1.7.2" : "2.3.1";
